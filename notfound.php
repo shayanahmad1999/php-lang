3e9f5f26d8cedce9d9b20f529bd5e390c97713e0
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-26 14:57:36 +0500

    add notfound file

---
+++ notfound.php
@@ -0,0 +1,13 @@
+<style>
+    h1 {
+        font-family: Tahoma;
+    }
+
+    h1 {
+        display: flex;
+        padding: 10px;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<h1>Page not Found...!</h1>
