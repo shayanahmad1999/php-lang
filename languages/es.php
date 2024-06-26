33d627a16e84dbc245fee5609537e15579b4d4e5
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-26 14:12:11 +0500

    spanish language set

---
+++ languages/es.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang = [
+    'Home' => 'Hogar',
+    'About us' => 'Sobre nosotras',
+    'Contact us' => 'Contacto',
+    'Login' => 'Acceso',
+    'Signup' => 'Inscribirse',
+    'Logout' => 'Cerrar sesión',
+    'Hi' => 'Hola',
+    'Languages' => 'Idiomas',
+    'Profile' => 'Perfil',
+    'Setting' => 'Configuración',
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' => 'Lorem Ipsum es simplemente un texto de relleno de la industria de la impresión y la tipografía. Lorem Ipsum ha sido el texto de relleno estándar de la industria desde el siglo XVI, cuando un impresor desconocido tomó una galería de tipos y los mezcló para hacer un libro de muestras tipográficas. Ha sobrevivido no sólo a cinco siglos, sino también al salto hacia la composición tipográfica electrónica, permaneciendo esencialmente sin cambios. Se popularizó en la década de 1960 con el lanzamiento de hojas de Letraset que contenían pasajes de Lorem Ipsum y, más recientemente, con software de autoedición como Aldus PageMaker, que incluía versiones de Lorem Ipsum.'
+];
