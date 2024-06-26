dfd741e47a234c186cef20ef91dfba9a68433e60
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-26 14:12:21 +0500

    french language set

---
+++ languages/fr.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang = [
+    'Home' => 'Maison',
+    'About us' => 'À propos de nous',
+    'Contact us' => 'Contactez-nous',
+    'Login' => 'Se connecter',
+    'Signup' => 'S\'inscrire',
+    'Logout' => 'Se déconnecter',
+    'Hi' => 'Salut',
+    'Languages' => 'Langues',
+    'Profile' => 'Profil',
+    'Setting' => 'Paramètre',
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' => "Lorem Ipsum est tout simplement un faux texte de l’industrie de l’imprimerie et de la composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, lorsqu'un imprimeur inconnu a pris une galerie de caractères et l'a brouillée pour en faire un livre de spécimens de caractères. Il a survécu non seulement à cinq siècles, mais aussi au saut vers la composition électronique, restant essentiellement inchangé. Il a été popularisé dans les années 1960 avec la sortie de feuilles Letraset contenant des passages de Lorem Ipsum, et plus récemment avec des logiciels de publication assistée par ordinateur comme Aldus PageMaker incluant des versions de Lorem Ipsum.",
+];
