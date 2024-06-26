7e77b2b292948ebc1bc3281a8d18c04f974a3fc7
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-26 14:13:48 +0500

    chinese language set

---
+++ languages/zh.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang = [
+    'Home' => '家',
+    'About us' => '关于我们',
+    'Contact us' => '联系我们',
+    'Login' => '登录',
+    'Signup' => '报名',
+    'Logout' => '登出',
+    'Hi' => '你好',
+    'Languages' => '语言',
+    'Profile' => '轮廓',
+    'Setting' => '环境',
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' => 'Lorem Ipsum 是印刷和排版行业的简单虚拟文本。自 1500 年代以来，Lorem Ipsum 一直是行业标准的虚拟文本，当时一位不知名的印刷商拿走了一堆字体并将其打乱以制作一本字体样本簿。它不仅经历了五个世纪的考验，而且跨越了电子排版的时代，基本保持不变。它在 20 世纪 60 年代随着包含 Lorem Ipsum 段落的 Letraset 表的发布而流行起来，最近又随着包含 Lorem Ipsum 版本的 Aldus PageMaker 等桌面出版软件而流行。',
+];
